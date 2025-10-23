960ed90444c2db3b32ccd79fb15c87573218584e
luna industries <lunaindustrieske@gmail.com>
2025-10-23 19:57:35 +0000

    remove the double navbar/header in all dashboards, only keep the main on

--- src/app/dashboards/layout.tsx
+++ src/app/dashboards/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from 'react';
-import { TopNav } from '@/components/top-nav';
 
 export default function DashboardLayout({
   children,
@@ -10,7 +9,6 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <TopNav />
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-background">
         {children}
       </main>
