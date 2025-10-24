ef2b259d9252f858cfb86680327edcb717d18d9b
luna industries <lunaindustrieske@gmail.com>
2025-10-24 13:16:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5267749204-831e7",
+  "appId": "1:284690700687:web:478969ca656e372eee13fa",
+  "apiKey": "AIzaSyDucCz6Y6xpCm5deFQR0MHwv2OXfVATQGY",
+  "authDomain": "studio-5267749204-831e7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "284690700687"
+};
