0c9c42f67037fd06a7ece56ab4b4d4cf993a7d20
Tradinta Commerce <tradintacommerce@gmail.com>
2025-10-28 12:59:55 +0000

    Runtime Error
    Error: Cannot read properties of undefined (reading 'fin

--- src/app/products/products-page-client.tsx
+++ src/app/products/products-page-client.tsx
@@ -64,7 +64,7 @@ import {
 import { Label } from '@/components/ui/label';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { RequestQuoteModal } from '@/components/request-quote-modal';
-import { type Product, type Manufacturer } from '@/lib/definitions';
+import { type Product, type Manufacturer } from '@/app/lib/definitions';
 import { categories } from '@/app/lib/categories';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { getAllProducts } from '@/app/lib/data';
